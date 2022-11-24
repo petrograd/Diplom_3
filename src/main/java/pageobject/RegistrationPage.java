4476364533d810ebd0ef2c92c5e92c9c7ca4d59b
Merkurev Petr <9662196@mail.ru>
2022-11-24 21:26:27 +0300

    Step anotations added to RegistrationPage

--- src/main/java/pageobject/RegistrationPage.java
+++ src/main/java/pageobject/RegistrationPage.java
@@ -1,5 +1,6 @@
 package pageobject;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -35,42 +36,52 @@ public class RegistrationPage extends BasicPages {
         super(driver);
     }
 
+    @Step("Получение URL страницы")
     public String getUrl() {
         return URL;
     }
 
+    @Step("Ждем отображения на странице кнопки Регистрация")
     public void waitRegButton() {
         waitForElement(completeRegistrationButton);
     }
 
+    @Step("Ввод данных в поле Имя")
     public void sendKeysName(String name) {
         nameInputField.sendKeys(name);
     }
 
+    @Step("Ввод данных в поле Email")
     public void sendKeysEmail(String email) {
         emailInput.sendKeys(email);
     }
 
+    @Step("Ввод данных в поле Пароль")
     public void sendKeysPassword(String password) {
         passwordInput.sendKeys(password);
     }
 
+    @Step("Жмем кнопку Регистрация")
     public void clickRegButton() {
         completeRegistrationButton.click();
     }
 
+    @Step("Ждем появления элемента \"Некорректный пароль\"")
     public void waitError() {
         waitForElement(passwordError);
     }
 
+    @Step("Получаем текст сообщения \"Некорректный пароль\"")
     public String getError() {
         return passwordError.getText();
     }
 
+    @Step("Жмем кнопку \"Войти\"")
     public void clickSignInLink() {
         signInLink.click();
     }
 
+    @Step("Регистрация аккаунта с данными")
     public void registerUser(String name, String email, String password) {
         sendKeysName(name);
         sendKeysEmail(email);
